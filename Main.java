72bfbadb7398e023f9ca1aee93fb1be29ded7f98
aayudeshk <aayudesh.kaparthi@patelhs.org>
2020-11-12 21:01:27 +0000

    I made a for loop for student names

--- Main.java
+++ Main.java
@@ -2,7 +2,13 @@ class Main {
   public static void main(String[] args) {
     // First we make an array for the Seat numbers
     int[] seatNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+    // Then we make another array for the student names.
     String[] studentNames = new String [15];
+    // Now we make a for loop for the student names.    
+    for(int seatNumbers <= [14]; )
+    {
+      System.out.print(studentNames[] + seatNumbers[]);
+    }
 
   }
 }
