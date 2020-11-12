556ca051b01570c0ef2ba701e5f763590c35aea7
aayudeshk <aayudesh.kaparthi@patelhs.org>
2020-11-12 20:51:52 +0000

    It should be student names not numbers

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 class Main {
   public static void main(String[] args) {
+    // First we make an array for the Seat numbers
     int[] seatNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-    String[] studentNumbers = new String [15];
+    String[] studentNames = new String [15];
+
   }
 }
